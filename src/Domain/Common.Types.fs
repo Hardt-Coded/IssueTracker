9503e45caab35a23c239b6dcbdf90681c52716a6
Daniel Hardt <daniel.hardt@docuware.com>
2019-11-05 18:02:30 +0100

    init

---
+++ src/Domain/Common.Types.fs
@@ -0,0 +1,53 @@
+﻿namespace Domain.Common
+
+open System.Text.RegularExpressions
+
+module Types =
+
+    type EMail = private EMail of string
+
+    type NotEmptyString = private NotEmptyString of string
+
+
+
+    module EMail =
+        
+        let private pattern = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$"
+        let private compiledRegex = 
+            Regex(pattern, 
+                RegexOptions.Compiled ||| 
+                RegexOptions.IgnoreCase ||| 
+                RegexOptions.ExplicitCapture)
+
+
+        let create email =
+            if compiledRegex.IsMatch(email) then
+                EMail email |> Ok
+            else
+                "invalid email adress" |> Error
+
+
+        let value (EMail email) = email
+
+
+    module NotEmptyString =
+
+        open System
+
+        let create label notEmptyStr =
+            if String.IsNullOrWhiteSpace(notEmptyStr) then
+                sprintf "%s must not be empty" label |> Error
+            else
+                NotEmptyString notEmptyStr |> Ok
+
+
+        let value (NotEmptyString str) = str
+                
+            
+        
+                
+            
+            
+
+    
+
---
+++ src/Domain/User.fs
@@ -0,0 +1,160 @@
+﻿namespace Domain
+
+open Common.Types
+
+module User =
+    
+    module CommandArguments =
+
+        type CreateUser = {
+            Name:string
+            EMail:string
+            Password:string
+        }
+
+        type DeleteUser = {
+            UserId:string
+        }
+
+        type ChangeEMail = {
+            UserId:string
+            EMail:string
+        }
+
+        type ChangePassword = {
+            UserId:string
+            Password:string
+        }
+
+        type AddToGroup = {
+            UserId:string
+            Group:string
+        }
+
+        type RemoveFromGroup = {
+            UserId:string
+            Group:string
+        }
+
+
+    type Command = 
+        | CreateUser of CommandArguments.CreateUser
+        | DeleteUser of CommandArguments.DeleteUser
+        | ChangeEMail of CommandArguments.ChangeEMail
+        | ChangePassword of CommandArguments.ChangePassword
+        | AddToGroup of CommandArguments.AddToGroup
+        | RemoveFromGroup of CommandArguments.RemoveFromGroup
+
+
+    [<AutoOpen>]
+    module Types =
+
+        open System
+
+        type UserId = private UserId of string
+
+        type HashPair = {
+            Hash:string
+            Salt:string
+        }
+
+        type PasswordHash = private PasswordHash of HashPair
+
+        module UserId =
+            
+            let create userId =
+                if String.IsNullOrWhiteSpace(userId) then
+                    sprintf "user id must not be empty" |> Error
+                else
+                    UserId userId |> Ok
+
+
+            let value (UserId userId) = userId
+
+
+        module PasswordHash =
+            
+            open System;
+            open System.Security.Cryptography
+            open Microsoft.AspNetCore.Cryptography.KeyDerivation
+
+            let create password =
+                if String.IsNullOrEmpty(password) then
+                    "password must not be empty" |> Error
+                else
+                    let salt = Array.zeroCreate 16
+                    use rng = RandomNumberGenerator.Create()
+                    rng.GetBytes(salt)
+                    let saltString = Convert.ToBase64String(salt)
+                    let hashed = 
+                        KeyDerivation.Pbkdf2(
+                            password = password,
+                            salt = salt,
+                            prf = KeyDerivationPrf.HMACSHA1,
+                            iterationCount = 10000,
+                            numBytesRequested = 256 / 8
+                        )
+                    let hashedString = Convert.ToBase64String(hashed)
+                    PasswordHash { Hash = hashedString; Salt = saltString } |> Ok
+
+                
+
+            let value (PasswordHash hashpair) = hashpair
+
+
+            
+
+
+    module EventArguments =
+
+        type UserCreated = {
+            Name:NotEmptyString
+            EMail:EMail
+            Password:PasswordHash
+        }
+
+        type UserDeleted = {
+            UserId:UserId
+        }
+
+        type EMailChanged = {
+            UserId:UserId
+            EMail:EMail
+        }
+
+        type PasswordChanged = {
+            UserId:UserId
+            Password:PasswordHash
+        }
+
+        type AddedToGroup = {
+            UserId:UserId
+            Group:NotEmptyString
+        }
+
+        type RemovedFromGroup = {
+            UserId:UserId
+            Group:NotEmptyString
+        }
+
+
+    type Event = 
+        | UserCreated of EventArguments.UserCreated
+        | UserDeleted of EventArguments.UserDeleted
+        | EMailChanged of EventArguments.EMailChanged
+        | PasswordChanged of EventArguments.PasswordChanged
+        | AddedToGroup of EventArguments.AddedToGroup
+        | RemovedFromGroup of EventArguments.RemovedFromGroup
+
+
+    type UserState = {
+        UserId:UserId
+        Name:NotEmptyString
+        EMail:EMail
+        Password:NotEmptyString
+    }
+
+
+
+
+
